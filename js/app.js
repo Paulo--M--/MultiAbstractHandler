72e19eadf7a6c9919be119801157923c9c4aaa13
malles <matthijs@bixie.nl>
2016-03-30 23:28:07 +0200

    create simple list

--- js/app.js
+++ js/app.js
@@ -5,8 +5,17 @@ jQuery(function () {
 
     new Vue({
         el: '#abstract-handler',
+
         data: {
             abstracts: []
+        },
+
+        methods: {
+            add: function () {
+                this.abstracts.push({
+                    AbstTitle: ''
+                });
+            }
         }
     });
 
