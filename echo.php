620b8f081a51ee301ffa28da9bbc998d1d6adeae
malles <matthijs@bixie.nl>
2016-03-31 00:04:41 +0200

    "save" abstracts

--- echo.php
+++ echo.php
@@ -1,3 +1,10 @@
 <?php
+$abstracts = [];
+foreach ($_POST['abstracts'] as $abstract) {
+	if ($abstract['AbstID'] == 0) {
+		$abstract['AbstID'] = rand(0,99999);
+	}
+	$abstracts[] = $abstract;
+}
 
-echo json_encode($_POST, JSON_NUMERIC_CHECK);
+echo json_encode(compact('abstracts'), JSON_NUMERIC_CHECK);
