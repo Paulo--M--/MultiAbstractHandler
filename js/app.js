e94cbec274f5fd70e07b43e1ddc1263dc43a99e7
malles <matthijs@bixie.nl>
2016-04-02 18:56:32 +0200

    add data from server

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ jQuery(function () {
             editor: {},
             editModal: {},
             editAbstract: false,
-            abstracts: []
+            abstracts: UIkit.$.merge([], window.$abstracts)
         },
 
         events: {
